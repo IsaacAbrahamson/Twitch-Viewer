846c6e7ef4f523907db7834cc6c73ef773c28eb7
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2022-08-07 21:27:53 -0400

    fix broken links and api

--- js/app.js
+++ js/app.js
@@ -7,28 +7,7 @@ main()
 
 
 async function loadStreamers() {
-  const streamers = [
-    "cretetion",
-    "freecodecamp",
-    "jonsandman",
-    "rocketleague",
-    "ESL_SC2",
-    "Bacon_Donut",
-    "highdistortion",
-    "crashcreeley",
-    "RobotCaleb",
-    "noobs2ninjas",
-    "QuickyBaby",
-    "terakilobyte",
-    "thomasballinger",
-    "comster404",
-    "deethane",
-    "ZeeooN",
-    "Wyld",
-    "BySliDe",
-    "scrubkillarl_",
-    "GronkhTV"
-  ]
+  const streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
   for (let streamer of streamers) {
     let data = await fetchData(streamer)
@@ -42,7 +21,7 @@ async function loadStreamers() {
 
     // invalid streamer
     if (channel.error) {
-      let logo = 'https://s10.postimg.org/rfebq9se1/random-user-icon-15571.png'
+      let logo = '../unknown.jpg'
       $('#invalid-streams').append(`
         <div class="stream-container">
           <div class="stream">
@@ -57,9 +36,10 @@ async function loadStreamers() {
 
     // currently streaming
     if (stream) {
-      let logo = stream.channel.logo || 'https://s10.postimg.org/rfebq9se1/random-user-icon-15571.png'
+      let logo = stream.channel.logo || '../unknown.jpg'
       let game = 'playing ' + stream.channel.game
-      if (game.length > 25) game.slice(0, 25).append('...')
+      if (game.length > 25)
+        game = game.slice(0, 25).concat('...')
 
       $('#online-streams').append(`
         <div class="stream-container">
@@ -75,7 +55,7 @@ async function loadStreamers() {
 
     // currently offline
     else {
-      let logo = channel.logo || 'https://s10.postimg.org/rfebq9se1/random-user-icon-15571.png'
+      let logo = channel.logo || '../unknown.jpg'
       $('#offline-streams').append(`
         <div class="stream-container">
           <div class="stream">
@@ -94,50 +74,50 @@ async function loadStreamers() {
 
 // $(document).ready(function() {
 //   window.onload = () => {
-    
-    
+
+
 //   }
 
 //   //Get data from Twitch api
-  
+
 
 //   streamers.forEach(streamer => {
 //     getStream(streamer)
 //   })
 
-  //Toggle menu on small devices
-  var toggle = false;
-  $('.toggle').click(function() {
-    $('#side-panel').css('width', '240px');
-    $('#content').css('width', 'calc(100% - 240px)');
-    $('#content').css('margin-left', '240px');
-    $('#side-panel *').css('display','block');
-    $('.toggle').css('display','none');
-    toggle = true;
-  });
-  $('#content').click(function() {
-    if (toggle === true) {
-      $('#side-panel').css('width', '40px');
-      $('#content').css('width', 'calc(100% - 40px)');
-      $('#content').css('margin-left', '40px');
-      $('#side-panel *').css('display','none');
-      $('.toggle').css('display','block');
-      toggle = false;
-    }
-  });
+//Toggle menu on small devices
+var toggle = false;
+$('.toggle').click(function () {
+  $('#side-panel').css('width', '240px');
+  $('#content').css('width', 'calc(100% - 240px)');
+  $('#content').css('margin-left', '240px');
+  $('#side-panel *').css('display', 'block');
+  $('.toggle').css('display', 'none');
+  toggle = true;
+});
+$('#content').click(function () {
+  if (toggle === true) {
+    $('#side-panel').css('width', '40px');
+    $('#content').css('width', 'calc(100% - 40px)');
+    $('#content').css('margin-left', '40px');
+    $('#side-panel *').css('display', 'none');
+    $('.toggle').css('display', 'block');
+    toggle = false;
+  }
+});
 
-  //Event handler for streamer panel
-  $(document.body).on('click', '.stream-container', function() {
-    if ($(this).parent().is('#offline-streams')) {
-      var streamer = $(this).find('.channel').text();
-      window.open('https://www.twitch.tv/' + streamer + '/profile', '_blank');
-    } else if ($(this).parent().is('#online-streams')) {
-      var streamer = $(this).find('.channel').text();
-      $('iframe').attr('src', 'https://player.twitch.tv/?channel=' + streamer);
-      $('#welcome').addClass('hidden');
-      $('#embed').removeClass('hidden');
-    }
-  });  
+//Event handler for streamer panel
+$(document.body).on('click', '.stream-container', function () {
+  if ($(this).parent().is('#offline-streams')) {
+    var streamer = $(this).find('.channel').text();
+    window.open('https://www.twitch.tv/' + streamer + '/profile', '_blank');
+  } else if ($(this).parent().is('#online-streams')) {
+    var streamer = $(this).find('.channel').text();
+    $('iframe').attr('src', 'https://player.twitch.tv/?channel=' + streamer + '&parent=twitch.iabrahamson.com');
+    $('#welcome').addClass('hidden');
+    $('#embed').removeClass('hidden');
+  }
+});
 
 //   function getStream(streamer) {
 //   }
--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,8 @@
 async function fetchData(streamer) {
   try {
     var data = await Promise.all([
-      fetch(`https://wind-bow.glitch.me/twitch-api/channels/${streamer}`).then((response) => response.json()),
-      fetch(`https://wind-bow.glitch.me/twitch-api/streams/${streamer}`).then((response) => response.json()),
+      fetch(`https://twitch-proxy.freecodecamp.rocks/twitch-api/channels/${streamer}`).then((response) => response.json()),
+      fetch(`https://twitch-proxy.freecodecamp.rocks/twitch-api/streams/${streamer}`).then((response) => response.json()),
     ])
     return data
   } catch (err) {
@@ -13,13 +13,13 @@ async function fetchData(streamer) {
 // From Chris Z, https://davidwalsh.name/document-readystate
 // utility for making sure document is ready
 HTMLDocument.prototype.ready = function () {
-	return new Promise(function(resolve, reject) {
-		if (document.readyState === 'complete') {
-			resolve(document);
-		} else {
-			document.addEventListener('DOMContentLoaded', function() {
-			    resolve(document);
-		    });
-		}
-    });
+  return new Promise(function (resolve, reject) {
+    if (document.readyState === 'complete') {
+      resolve(document);
+    } else {
+      document.addEventListener('DOMContentLoaded', function () {
+        resolve(document);
+      });
+    }
+  });
 }
